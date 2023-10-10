de136ae2205c5a529bd0473acbc9ed72a0660399
thisjunghyewon <this.junghyewon@gmail.com>
2023-10-10 11:45:33 +0200

    Create chat app

---
+++ components/Chat.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Chat = ({ route, navigation }) => {
+  const { name, color } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
+
+  return (
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Chat;
