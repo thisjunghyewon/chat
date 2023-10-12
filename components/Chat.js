7035b9d459c338f2ed898d607b0b07edfc9445de
thisjunghyewon <this.junghyewon@gmail.com>
2023-10-12 19:08:16 +0200

    Real-Time Applications & Data Storage

--- components/Chat.js
+++ components/Chat.js
@@ -1,64 +1,52 @@
 import { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params;
+const Chat = ({ route, navigation, db }) => {
+  const { name, color, userID } = route.params;
+  console.log(route.params);
   const [messages, setMessages] = useState([]);
   //A chat app needs to send, receive, and display messages,
   //so it makes sense to add messages as a state
 
-  /*called with a callback function passed into it.
-  A way to access the latest state value*/
-  /*append() function provided by GiftedChat, which appends the new message to the newMessages array. * */
-  const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
-  };
-
-  const renderBubble = (props) => {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "#000",
-          },
-          left: {
-            backgroundColor: "#FFF",
-          },
-        }}
-      />
-    );
-  };
-
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+    const unsubMessages = onSnapshot(
+      query(collection(db, "messages"), orderBy("createdAt", "desc")),
+      (documentsSnapshot) => {
+        let newMessages = [];
+        documentsSnapshot.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+        });
+        setMessages(newMessages);
+      }
+    );
+
+    return () => {
+      if (unsubMessages) unsubMessages();
+    };
   }, []);
+
+  const onSend = (newMessages = []) => {
+    const messageToSave = {
+      ...newMessages[0],
+      user: {
+        _id: userID,
+        name: name,
+      },
+    };
+    addDoc(collection(db, "messages"), messageToSave);
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -69,15 +57,32 @@ const Chat = ({ route, navigation }) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
-        onSend={(messages) => onSend(messages)} //onSend() function is called when a user sends a message.
+        onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1,
+          _id: userID,
+          name: name,
         }}
       />
       {Platform.OS === "android" ? (
         <KeyboardAvoidingView behavior="height" />
       ) : null}
     </View>
+  );
+};
+
+const renderBubble = (props) => {
+  return (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000",
+        },
+        left: {
+          backgroundColor: "#FFF",
+        },
+      }}
+    />
   );
 };
 
